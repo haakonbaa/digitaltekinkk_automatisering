83dd1ef96cd3df1fa6356f8573f240776a02a1fd
TorBorve <rasmustor18@gmail.com>
2020-11-25 17:09:49 +0100

    Lagt til mogelegheita for don't care veridar

--- digtek/methods/raw/Tabellmetoden.py
+++ digtek/methods/raw/Tabellmetoden.py
@@ -1,17 +1,21 @@
-#Dette er eit program som forenklar ein funksjon gitt på kanonisk form ved hjelp av tabell metoden
+# Dette er eit program som forenklar ein funksjon gitt på kanonisk form ved hjelp av tabell metoden
 
-def sannhetstabell(variablar, mintermar): #Lager sannhetstabell for ein funskjon vha. mintermane til funksjonen og antal variablar
-    tabell = [0 for i in range(2**variablar)]
+def sannhetstabell(variablar,
+                   mintermar):  # Lager sannhetstabell for ein funskjon vha. mintermane til funksjonen og antal variablar
+    tabell = [0 for i in range(2 ** variablar)]
     for minterm in mintermar:
         tabell[minterm] = 1
     return tabell
 
-def F(lista, variablar, func): #Definerer den boolske funksjonen ut frå input func og lista med verdi på variablane
+
+def F(lista, variablar, func):  # Definerer den boolske funksjonen ut frå input func og lista med verdi på variablane
     for i in range(len(variablar)):
         exec("%s = %d" % (variablar[i], lista[i]))
     return int(eval(func))
 
-def konverterNotasjon(variablar, boolsk): #Konverterer til notasjon av boolske funksjonar som python bruker. Fungerer i utgangspunket berre på output frå tabellmetoden!
+
+def konverterNotasjon(variablar,
+                      boolsk):  # Konverterer til notasjon av boolske funksjonar som python bruker. Fungerer i utgangspunket berre på output frå tabellmetoden!
     boolsk = boolsk.split()
     svar = ""
     for utrykk in boolsk:
@@ -21,39 +25,25 @@ def konverterNotasjon(variablar, boolsk): #Konverterer til notasjon av boolske f
             delsvar = ""
             indexVariablar = []
             for i in range(len(utrykk)):
-                for j in range(len(utrykk)-i):
-                    if utrykk[j:j+i+1] in variablar:
-                        indexVariablar.append((j+i, utrykk[j:j+i+1]))
+                for j in range(len(utrykk) - i):
+                    if utrykk[j:j + i + 1] in variablar:
+                        indexVariablar.append((j + i, utrykk[j:j + i + 1]))
 
-            indexVariablar.sort(key= lambda x: x[0])
+            indexVariablar.sort(key=lambda x: x[0])
             for variabel in indexVariablar:
                 delsvar += " and"
                 try:
-                    if utrykk[variabel[0]+1] == "'":
+                    if utrykk[variabel[0] + 1] == "'":
                         delsvar += " not " + variabel[1]
                     else:
                         delsvar += " " + variabel[1]
                 except:
                     delsvar += " " + variabel[1]
             svar += delsvar[4:]
-            """  #Fungerer kun med variablar med lengd 1     
-            for i in range(len(utrykk)-1):
-                if utrykk[i] in variablar:
-
-                    delsvar += " and"
-                    if utrykk[i+1] == "'":
-                        delsvar += " not " + utrykk[i]
-                    else:
-                        delsvar += " " + utrykk[i]
-                else:
-                    if utrykk[i:i+1]
-            if utrykk[-1] in variablar:
-                delsvar += " and " + utrykk[-1]
-            svar += delsvar[4:]
-            """
     return svar
 
-def sjekkLike(variablar, mintermar, uttrykk): #Sjekker om funskjon gitt med mintermar er lik funskjon på standarform
+
+def sjekkLike(variablar, mintermar, uttrykk):  # Sjekker om funskjon gitt med mintermar er lik funskjon på standarform
     func = konverterNotasjon(variablar, uttrykk)
     fasit = sannhetstabell(len(variablar), mintermar)
     svar = []
@@ -65,7 +55,8 @@ def sjekkLike(variablar, mintermar, uttrykk): #Sjekker om funskjon gitt med mint
         return True
     return False
 
-def sjekkbar(variablar): #Sjekker om ein variabel er ein substring av ein annan
+
+def sjekkbar(variablar):  # Sjekker om ein variabel er ein substring av ein annan
     for i in range(len(variablar)):
         for j in range(len(variablar)):
             if j == i:
@@ -74,27 +65,31 @@ def sjekkbar(variablar): #Sjekker om ein variabel er ein substring av ein annan
                 return False
     return True
 
+
 def dec2binary(dec, length):  # gjer eit desimalt tal om til binært tal med lengda length
     binary = str(bin(dec)[2:])
     binary = "0" * (length - len(binary)) + binary
     return binary
 
-def antalEin(binary): #Returnerer antal eittal i binært tal 
+
+def antalEin(binary):  # Returnerer antal eittal i binært tal
     antal = 0
     for siffer in binary:
         if siffer == "1":
             antal += 1
     return antal
 
-def mintermar2tabell(mintermar): #Tar inn mintermar ag gjere det om til tabell slik som i tabellmetoden
+
+def mintermar2tabell(mintermar):  # Tar inn mintermar ag gjere det om til tabell slik som i tabellmetoden
     tabell = []
     for minterm in mintermar:
         binary = dec2binary(minterm, len(variablar))
         tabell.append([antalEin(binary), tuple([minterm]), binary, False])
     return tabell
 
-def maketabell(tabellMintermar): 
-    tabell = [None for i in range(len(variablar)+1)]
+
+def maketabell(tabellMintermar):
+    tabell = [None for i in range(len(variablar) + 1)]
 
     for minterm in tabellMintermar:
         if tabell[minterm[0]]:
@@ -104,7 +99,8 @@ def maketabell(tabellMintermar):
 
     return tabell
 
-def forkjellEin(bin1, bin2): #returnerer forenkla binært tal dersom forskjellen er ein ellers None
+
+def forkjellEin(bin1, bin2):  # returnerer forenkla binært tal dersom forskjellen er ein ellers None
     forskjell = 0
     for i in range(len(bin1)):
         if bin1[i] != bin2[i]:
@@ -118,25 +114,27 @@ def forkjellEin(bin1, bin2): #returnerer forenkla binært tal dersom forskjellen
         return forenkla
     return None
 
-def tabellmetoden(tabell): #reknar ut neste tabell
-    nyTabell = [None for i in range(len(tabell)-1)]
-    for i in range(len(tabell)-1):
-        if not tabell[i] or not tabell[i+1]:
+
+def tabellmetoden(tabell):  # reknar ut neste tabell
+    nyTabell = [None for i in range(len(tabell) - 1)]
+    for i in range(len(tabell) - 1):
+        if not tabell[i] or not tabell[i + 1]:
             continue
         for q in range(len(tabell[i])):
-            for w in range(len(tabell[i+1])):
+            for w in range(len(tabell[i + 1])):
                 bin1 = tabell[i][q][2]
-                bin2 = tabell[i+1][w][2]
+                bin2 = tabell[i + 1][w][2]
                 if forkjellEin(bin1, bin2):
                     tabell[i][q][3] = True
-                    tabell[i+1][w][3] = True
+                    tabell[i + 1][w][3] = True
                     if nyTabell[i]:
-                        nyTabell[i].append([i, tabell[i][q][1] + tabell[i+1][w][1], forkjellEin(bin1, bin2), False])
+                        nyTabell[i].append([i, tabell[i][q][1] + tabell[i + 1][w][1], forkjellEin(bin1, bin2), False])
                     else:
-                        nyTabell[i] = [[i, tabell[i][q][1] + tabell[i+1][w][1], forkjellEin(bin1, bin2), False]]
+                        nyTabell[i] = [[i, tabell[i][q][1] + tabell[i + 1][w][1], forkjellEin(bin1, bin2), False]]
     return nyTabell, tabell
 
-def printTabell(tabell): #printer tabellen på eit "fint" format
+
+def printTabell(tabell):  # printer tabellen på eit "fint" format
     maxSub = len("Subkube")
     maxVerdi = len("Verdi")
     for i in range(len(tabell)):
@@ -151,18 +149,19 @@ def printTabell(tabell): #printer tabellen på eit "fint" format
 
     lengdTabell = len("|Gruppa|  |  | Dekka |") + maxSub + maxVerdi
 
-    print("-"*lengdTabell)
+    print("-" * lengdTabell)
     print("|Gruppa| " + "Subkube".ljust(maxSub) + " | " + "Verdi".ljust(maxVerdi) + " | Dekka |")
-    print("-"*lengdTabell)
+    print("-" * lengdTabell)
     for i in range(len(tabell)):
         if tabell[i]:
             for minterm in tabell[i]:
-                print("|   " + str(minterm[0]) + "  | " + str(minterm[1]).ljust(maxSub) + " | " + str(minterm[2]).ljust(maxVerdi) + " | ", end= "")
+                print("|   " + str(minterm[0]) + "  | " + str(minterm[1]).ljust(maxSub) + " | " + str(minterm[2]).ljust(
+                    maxVerdi) + " | ", end="")
                 if minterm[3]:
                     print(str(minterm[3]) + "  |")
                 else:
                     print(str(minterm[3]) + " |")
-                print("-"*lengdTabell)
+                print("-" * lengdTabell)
     print()
 
 
@@ -177,7 +176,7 @@ def printPI(PI, mintermar):
         if len(str(primImp[1])) > maxminterm:
             maxminterm = len(str(primImp[1]))
 
-    essensiel = [] #Finn mintermane som berre er dekka av ein primimplikant
+    essensiel = []  # Finn mintermane som berre er dekka av ein primimplikant
     for minterm in mintermar:
         antal = 0
         for primImp in PI:
@@ -185,16 +184,16 @@ def printPI(PI, mintermar):
                 antal += 1
         if antal == 1:
             essensiel.append(minterm)
-    
+
     mintermar.sort()
     maxsiffer = len(str(mintermar[-1]))
-    lengd = len("|  |  |") + len(mintermar)*(3 + maxsiffer) + len(" Valgt |") + maxminterm + maxuttrykk
-    print("-"*lengd)
-    print("| " + "Uttrykk".ljust(maxuttrykk)+ " | " + "Mintermar".ljust(maxminterm) + " |", end="")
+    lengd = len("|  |  |") + len(mintermar) * (3 + maxsiffer) + len(" Valgt |") + maxminterm + maxuttrykk
+    print("-" * lengd)
+    print("| " + "Uttrykk".ljust(maxuttrykk) + " | " + "Mintermar".ljust(maxminterm) + " |", end="")
     for minterm in mintermar:
         print(" " + str(minterm).ljust(maxsiffer) + " |", end="")
     print(" Valgt |")
-    print("-"*lengd)
+    print("-" * lengd)
     for primImp in PI:
         boolsk = bin2boolsk(primImp[2])
         print("| " + boolsk.ljust(maxuttrykk) + " | " + str(primImp[1]).ljust(maxminterm) + " |", end="")
@@ -210,11 +209,11 @@ def printPI(PI, mintermar):
             print(" True  |")
         else:
             print(" False |")
-        print("-"*lengd)
+        print("-" * lengd)
     print()
 
 
-def essensielPI(PI, mintermar): #Finner essensielle primimplikantar. Endrer så veriden til True
+def essensielPI(PI, mintermar):  # Finner essensielle primimplikantar. Endrer så veriden til True
     for minterm in mintermar:
         dekkt = 0
         for i in range(len(PI)):
@@ -225,21 +224,23 @@ def essensielPI(PI, mintermar): #Finner essensielle primimplikantar. Endrer så 
             PI[primImp][3] = True
     return PI
 
-def delDuplicat(PI): #Fjerner duplikat av primimplikantar
+
+def delDuplicat(PI):  # Fjerner duplikat av primimplikantar
     kopi = [x[:] for x in PI]
     """for i in range(len(PI)-1):
         for q in range(i+1, len(PI)):
             if PI[i][2] == PI[q][2]:
                 print(PI[i])
                 kopi.remove(PI[i])"""
-    for i in range(len(PI)-1,0,-1):
-        for q in range(i-1,-1,-1):
+    for i in range(len(PI) - 1, 0, -1):
+        for q in range(i - 1, -1, -1):
             if PI[i][2] == PI[q][2]:
                 kopi.pop(i)
                 break
     return kopi
 
-def irredudant(PI, mintermar): #finner irredudant dekkning ved å velge dei mintermane som dekke flest udekka mintermar
+
+def irredudant(PI, mintermar):  # finner irredudant dekkning ved å velge dei mintermane som dekke flest udekka mintermar
     uttrykk = []
     mintermar = set(mintermar)
     dekkt = None
@@ -266,7 +267,7 @@ def irredudant(PI, mintermar): #finner irredudant dekkning ved å velge dei mint
     return uttrykk
 
 
-def irredudant2boolsk(irredudant): #Gjer irredudant dekning om til boolskfunksjon
+def irredudant2boolsk(irredudant):  # Gjer irredudant dekning om til boolskfunksjon
     boolsk = ""
     for primImp in irredudant:
         uttrykk = ""
@@ -279,6 +280,7 @@ def irredudant2boolsk(irredudant): #Gjer irredudant dekning om til boolskfunksjo
         boolsk += uttrykk
     return boolsk[:-3]
 
+
 def bin2boolsk(binstr):
     boolsk = ""
     for i in range(len(binstr)):
@@ -288,17 +290,14 @@ def bin2boolsk(binstr):
             boolsk += variablar[i] + "'"
     return boolsk
 
-
-def main():
-    global variablar
-    variablar = input("Skriv inn variablar separert med mellomrom: ").split()
+def getMintermar():
     while True:
         mintermar = input("Skriv inn mintermar separert med mellomrom: ").split()
         try:
             ugyldig = False
             mintermar = list(map(lambda minterm: int(minterm), mintermar))
             for minterm in mintermar:
-                if minterm < 0 or minterm >= 2**len(variablar):
+                if minterm < 0 or minterm >= 2 ** len(variablar):
                     print("Ugyldig mintermar")
                     ugyldig = True
                     break
@@ -310,14 +309,48 @@ def main():
             break
         except:
             print("mintermane må vere eit tal")
+    while True:
+        dCare = input("Skriv inn don't care separert med mellomrom: ").split()
+        try:
+            ugyldig = False
+            dCare = list(map(lambda minterm: int(minterm), dCare))
+            for minterm in dCare:
+                if minterm < 0 or minterm >= 2 ** len(variablar):
+                    print("Ugyldig don't care")
+                    ugyldig = True
+                    break
+            if ugyldig:
+                continue
+            if len(dCare) != len(set(dCare)):
+                print("Du skreiv inn ein don't care minterm fleire gongar")
+                continue
+            if len(set(dCare).intersection(set(mintermar))) >= 1:
+                print("Ein minterm kan ikkje vere både minterm og don't care")
+                continue
+            break
+        except:
+            print("Don't care må vere eit tal")
+    return mintermar, dCare
 
+def dCarePI(PI, dCare):
+    for i in range(len(PI)):
+        PI[i][1] = tuple(set(PI[i][1]).union(set(dCare)))
+    return PI
+
+def main():
+    global variablar
+    variablar = input("Skriv inn variablar separert med mellomrom: ").split()
+    mintermar, dCare = getMintermar()
+    mintermar += dCare
     tabellMintermar = mintermar2tabell(mintermar)
-    tabellMintermar.sort(key= lambda minterm: minterm[0])
+    tabellMintermar.sort(key=lambda minterm: minterm[0])
     nyTabell = maketabell(tabellMintermar)
-    PI = [] #Prim implikantar
+    PI = []  # Prim implikantar
     print("\nTabellar:")
-    while True: #Kjører tabellmetoden til det ikkje går an å forenkla meir
-        nyTabell,tabell = tabellmetoden(nyTabell)
+    if len(dCare) != 0:
+        print("For å løyse med don't care så antar me at dei er ein og fjernar seinare")
+    while True:  # Kjører tabellmetoden til det ikkje går an å forenkla meir
+        nyTabell, tabell = tabellmetoden(nyTabell)
         if nyTabell == [None for i in range(len(nyTabell))]:
             for i in range(len(tabell)):
                 if tabell[i]:
@@ -331,7 +364,7 @@ def main():
                     if not tabell[i][q][3]:
                         PI.append(tabell[i][q])
         printTabell(tabell)
-
+    PI = dCarePI(PI, dCare)
     PI = delDuplicat(PI)
     PI = essensielPI(PI, mintermar)
     print("Tabell for å finna minimaldekkning:")
@@ -351,5 +384,6 @@ def main():
     else:
         print("Kan ikkje sjekka om forenklinga er riktig.")
         print("Grunna ein variabel var substring av ein annan")
+
 
 main()
